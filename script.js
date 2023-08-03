6688216af1edc16cc968b249ee7d060fc7656962
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-08-03 19:18:02 +0530

    Everything related to Format

--- script.js
+++ script.js
@@ -1,120 +1,112 @@
+// Accessing all elements by their IDs
+const rock = document.getElementById('Rock');
+const paper = document.getElementById('Paper');
+const scissors = document.getElementById('Scissors');
+const player = document.getElementById('player-select');
+const computer = document.getElementById('computer-select');
+const button = document.querySelectorAll('.btns');
+const statusd = document.getElementById('status');
+const yourScore = document.getElementById('span');
+const newGame = document.getElementById('new');
 
-// accessing all id's
-const rock = document.getElementById('Rock')
-const paper = document.getElementById('Paper')
-const scissors = document.getElementById('Scissors')
-const player = document.getElementById('player-select')
-const computer = document.getElementById('computer-select')
-const button = document.querySelectorAll('.btns')
-const statusd = document.getElementById('status')
-const yourScore = document.getElementById('span')
-const newGame = document.getElementById('new')
+// Images for rock, paper, and scissors
+const rock_img = "Rock.png";
+const paper_img = "Paper.png";
+const scissors_img = "Scissors.png";
 
+// Object to track scores
+let trackScore = { 'computerScore': 0, 'playerScore': 0 };
 
-const rock_img = "Rock.png"
-const paper_img = "Paper.png"
-const scissors_img = "Scissors.png"
+// Array of rock, paper, and scissors
+const rps = ['Rock', 'Paper', 'Scissor'];
 
-let trackScore = {'computerScore':0, 'playerScore':0}
-
-rps = ['Rock', 'Paper', 'Scissor']
-
-// button clicks
+// Event listeners for button clicks
 rock.onclick = () => {
-  player.src = rock_img
-  let playerChoice = 'Rock'
-  calling(playerChoice)
+  player.src = rock_img;
+  let playerChoice = 'Rock';
+  calling(playerChoice);
 }
 
 paper.onclick = () => {
-  player.src = paper_img
-  let playerChoice = 'Paper'
-  calling(playerChoice)
+  player.src = paper_img;
+  let playerChoice = 'Paper';
+  calling(playerChoice);
 }
 
 scissors.onclick = () => {
-  player.src = scissors_img
-  let playerChoice = 'Scissors'
-  calling(playerChoice)
+  player.src = scissors_img;
+  let playerChoice = 'Scissors';
+  calling(playerChoice);
 }
 
-// randomly computer selects 
+// Randomly selects computer choice
 const getComputerChoice = (rps) => {
-  const random = Math.floor( Math.random() * rps.length);
+  const random = Math.floor(Math.random() * rps.length);
   let computerChoice1 = rps[random];
   return computerChoice1;
 }
 
-
-// updating computer choice to DOM
+// Updates computer choice to the DOM
 const auto = (computerChoice) => {
-  if ( computerChoice == 'Rock') {
-    computer.src = rock_img
-  } else if ( computerChoice == 'Paper') {
-    computer.src = paper_img
+  if (computerChoice === 'Rock') {
+    computer.src = rock_img;
+  } else if (computerChoice === 'Paper') {
+    computer.src = paper_img;
   } else {
-    computer.src = scissors_img
+    computer.src = scissors_img;
   }
-
 }
 
-// calling score & auto functions
+// Calls score & auto functions
 const calling = (playerChoice) => {
- const computerChoice = getComputerChoice(rps)
- auto(computerChoice)
- let score =  makeScore(playerChoice,computerChoice ) 
- updates(score)
- trackScore['playerScore'] += score
- yourScore.innerText = trackScore['playerScore']
-
+  const computerChoice = getComputerChoice(rps);
+  auto(computerChoice);
+  let score = makeScore(playerChoice, computerChoice);
+  updates(score);
+  trackScore['playerScore'] += score;
+  yourScore.innerText = trackScore['playerScore'];
 }
 
-
-// finding player score
-const makeScore = (playerChoice, computerChoice ) => {
+// Calculates player score
+const makeScore = (playerChoice, computerChoice) => {
   let score;
 
   // All situations where human draws, set `score` to 0
   if (playerChoice === computerChoice) {
-    score = 0
-  
+    score = 0;
   } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
-    score = 1
-
+    score = 1;
   } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-    score = 1
-
+    score = 1;
   } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-    score = 1
-
-  // Otherwise human loses (aka set score to -1)
+    score = 1;
   } else {
-    score = -1
+    // Otherwise human loses (aka set score to -1)
+    score = -1;
   }
-  return score
+  return score;
 }
 
+// Updates the status message based on the score
 const updates = (score) => {
-  if(score == 1) {
-    statusd.style.color = "lightgreen"
-    statusd.innerText = "You win..."
-  } else if ( score == 0) {
-    statusd.style.color = "yellow"
-    statusd.innerText = "Draw..."
-
+  if (score === 1) {
+    statusd.style.color = "lightgreen";
+    statusd.innerText = "You win...";
+  } else if (score === 0) {
+    statusd.style.color = "yellow";
+    statusd.innerText = "Draw...";
   } else {
-    statusd.style.color = "red"
-    statusd.innerText = "Computer Win.."
+    statusd.style.color = "red";
+    statusd.innerText = "Computer Win..";
   }
 }
 
-
-// clear all
+// Event listener for "New Game" button click
 newGame.onclick = () => {
-  player.src = scissors_img
-  computer.src = scissors_img
-  statusd.innerText = "Start.."
-  trackScore['playerScore'] = 0
-  statusd.style.color = "white"
-  yourScore.innerText = trackScore['playerScore']
+  player.src = scissors_img;
+  computer.src = scissors_img;
+  statusd.innerText = "Start..";
+  trackScore['playerScore'] = 0;
+  statusd.style.color = "white";
+  yourScore.innerText = trackScore['playerScore'];
 }
